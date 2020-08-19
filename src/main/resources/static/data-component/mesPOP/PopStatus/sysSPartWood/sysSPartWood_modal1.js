811902f855d84befedc29803725813d2ecfbeb67
heojiyoon <hjygfgf@tobesystem.co.kr>
2020-08-19 18:00:16 +0900

    태창 생산관리 하위메뉴 고객지급품관리 (미완)

--- src/main/resources/static/data-component/mesPOP/PopStatus/sysSPartWood/sysSPartWood_modal1.js
+++ src/main/resources/static/data-component/mesPOP/PopStatus/sysSPartWood/sysSPartWood_modal1.js
@@ -8,8 +8,39 @@ function modal_start1() {
 
 ////////////////////////////클릭 함수/////////////////////////////////////
 // 키워드를 통한 저장,수정  INSERT-I , UPDATE-U
-function addUdate_btn() {
+function addUpdate_btn() {
+    if (confirm('내용을 수정하시겠습니까?')) {
+        var add_data = value_return(".modal_value");
+        var formData = new FormData();
+        var check;
 
+        formData.append("supp_code", add_data.supp_code);
+        formData.append("part_kind", add_data.part_kind);
+        formData.append("part_code", add_data.part_code);
+        formData.append("remark", add_data.remark);
+
+        if ($("#file_02").prop("files")[0] == null) {
+            check = 0;
+            formData.append("check", check);
+        } else {
+            check = 1;
+            formData.append("file3", $("#file_03").prop("files")[0]);
+            formData.append("check", check);
+        }
+        // wrapWindowByMask2();
+        // ccn_file_ajax("/sysSPartDrawingAdd", formData).then(function (data) {
+        //     if (data.result === 'NG') {
+        //         alert(data.message);
+        //     }
+        //     closeWindowByMask();
+        //     $("#addDialog").dialog('close');
+        //     $('#mes_grid').trigger('reloadGrid')
+        //
+        // }).catch(function (err) {
+        //     closeWindowByMask();
+        //     $("#addDialog").dialog('close');
+        // });
+    }
 }
 ////////////////////////////호출 함수/////////////////////////////////////
 function msg_get_modal1() {
@@ -18,44 +49,52 @@ function msg_get_modal1() {
     msgGet_auth("TBMES_E008");// 데이터 등록 실패
 }
 
+function file_change(e) {
+    var filename = $(e).val().split('\\');
+    var data = $(e).val().split('.'); // 확장자
+    if ( $(e).val() !== ''){
 
+        $(e).closest("div")
+            .children(".file_labal")
+            .text("업로드완료");
+    }
+}
 
-function modal_make1() { //dialog 에 사이즈 및 버튼 기타옵션을 설정해준다
+function modal_make1() {
     $("#addDialog").dialog({
-        modal: true, // 모달 설정 ( 뒷배경 클릭 방지 마스크로 덮음)
-        width: 550, // 가로 설정
-        height: 'auto', //세로 설정
-        autoOpen: false, //자동 오픈 해제
-        resizable: false, // 크기 조절 불가설정
-        buttons: [ // 모달 하단 버튼 설정
+        modal: true,
+        width: 550,
+        height: 'auto',
+        autoOpen: false,
+        resizable: false,
+        buttons: [
             {
-                text: '저장',
+                text: "저장",
                 'class': 'btn btn-primary btn-minier',
                 click: function () {
-                    $(this).dialog("close");
-
+                    addUpdate_btn();
                 }
-
             },
             {
-                text: '삭제',
-                'class': 'btn btn-primary btn-minier',
+                text: "취소",
+                'class': 'btn btn-minier',
                 click: function () {
                     $(this).dialog("close");
-
                 }
-
             }
-
         ]
-    })
+    });
 }
 
 function select_modale_box(){
-    $('#select_modal1').select2();
-    $('#select_modal2').select2();
+    select_makes_base("#supp_select_modal","/suppAllGet","supp_code","supp_name",{keyword:'Y',keyword2:'CORP_TYPE2'},"Y").then(function (data) {
+        $('#part_kind_select').empty();
+        var option = $("<option></option>").text('전체').val('');
+        $('#part_kind_select').append(option);
+        $('#part_kind_select').select2();
+    });
 }
 
 function datepickerInput() {
-    datepicker_makes("#datepicker_modal1", 0);
+    datepicker_makes("#wood_in_date_modal1", 0);
 }

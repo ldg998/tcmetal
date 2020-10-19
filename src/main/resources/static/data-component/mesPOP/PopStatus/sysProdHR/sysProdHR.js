09def321e50c390040a5cc1bbfaaf62a92f7fd79
donggunDi <ldg998@tobesystem.co.kr>
2020-10-19 19:37:44 +0900

    인보이스 화면 변경 기간별생산실적 공용코드 날짜2 수정 리드타임 프로시져 수정

--- src/main/resources/static/data-component/mesPOP/PopStatus/sysProdHR/sysProdHR.js
+++ src/main/resources/static/data-component/mesPOP/PopStatus/sysProdHR/sysProdHR.js
@@ -333,7 +333,7 @@ function jqGrid_main() {
 }
 
 function select_box() {
-    select_makes_base("#select1", "/sysCommonAllGet", "code_value", "code_name1", {keyword: 'LINE_GROUP'}, '').then(function (data) {
+    select_makes_base("#select1", "/sysLineGroupAllGet","code_value","code_name1",{keyword:'4'},'').then(function (data) {
         select_makes_base("#select2", "/syslineAllGroupGet", "line_code", "line_name", {keyword: data[0].code_value}, '').then(function (data2) {
         });
     });
